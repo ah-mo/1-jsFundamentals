0dabf07c19d7c000271e55394e5295f55b9a4965
Austin Morse <morse@prime-research.com>
2020-08-18 23:11:54 -0500

    second commit

--- 1-jsFundamentals/1-grammarAndTypes/04-scope.js
+++ 1-jsFundamentals/1-grammarAndTypes/04-scope.js
@@ -38,3 +38,77 @@ for (item in object) {
 console.log(item);
 
 //no keyword usually defaults to var
+
+/*SCOPE
+
+What is scope?
+JS has both LOCAL and GLOBAL scope
+*/
+
+//example 1
+var x = 12;
+
+function scope(){
+    var x = 33;
+    //var y = 10;
+    console.log(x);
+}
+scope(); //logs 33
+//console.log(y); //undefined because it's not globally scoped 
+console.log(x); //logs 12
+
+//example 2
+var x = 12;
+
+function scope(){
+    x = 33;
+    console.log(x);
+}
+scope(); //33 because the local change affects what happens later
+console.log(x); //33 because it has been affected
+
+//example 3:
+var x = 1;
+
+function scope(){
+    var x = 2;
+    function scope(){
+        var x = 3;
+        console.log(x); //3
+    }
+    scope();
+    console.log(x); //2
+}
+scope();
+console.log(x); //1
+
+
+//example 4 contrasts with 5:
+var x = 12;
+
+function scope(){
+    var x = 33;
+    if (true){
+        let x = 45;
+        console.log(x); //45
+    }
+    console.log(x); //33
+}
+
+scope();
+console.log(x); //12
+
+//example 5 contrasts with 4:
+var x = 12;
+
+function scope(){
+    var x = 33;
+    if (true){
+        var x = 45;
+        console.log(x); //45
+    }
+    console.log(x); //45 -> var doesn't obey 'block' scope
+}
+
+scope();
+console.log(x); //12
--- 1-jsFundamentals/1-grammarAndTypes/05-hoisting.js
+++ 1-jsFundamentals/1-grammarAndTypes/05-hoisting.js
@@ -20,3 +20,37 @@ newFunction();
 let newFunction = () => {
     console.log('I have not been hoisted!')
 }
+
+/*
+HOISTING
+
+*/
+
+//example 1:
+console.log(scissors);
+var scissors = 'blue';
+
+//example 1 breakdown: all variables and functions are pulled to the top automatically
+var scissors;
+console.log(scissors);
+scissors = 'blue';
+
+//example 2:
+function hoistTest(){
+    console.log(testVar);
+    var testVar = 10;
+    console.log(testVar);
+}
+
+hoistTest();
+
+//example 2 breakdown:
+
+function hoistTest(){
+    var testVar;
+    console.log(testVar);
+    testVar = 10;
+    console.log(testVar);
+}
+
+hoistTest();
