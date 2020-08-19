94f14d12ac2a2cb09795a816ed93f8cd27371d0d
Austin Morse <morse@prime-research.com>
2020-08-18 23:41:46 -0500

    third commit

--- 1-jsFundamentals/5-functions/05-arrowFunctions.js
+++ 1-jsFundamentals/5-functions/05-arrowFunctions.js
@@ -15,3 +15,52 @@ hello('austin');
 
 let hey = () => console.log('hey');
 hey();
+
+//normal function declaration
+function coffee(){
+    return 'coffee is good';
+}
+
+//normal function expression
+let tea = function(){
+    return 'tea is healthy';
+}
+
+console.log(coffee());
+console.log(tea());
+
+//arrow functions are *always* anonymous
+//arrow function expression 
+//ARROW FUNCTIONS DO NOT GET HOISTED
+
+let hotChocolate = () => {
+    return 'hot chocolate is king';
+};
+
+console.log(hotChocolate());
+
+let animals = (kittens, puppies) => {
+    return `I have ${kittens} cat(s) and ${puppies} dog(s)`;
+};
+
+console.log(animals(3,2));
+console.log(animals(0,0));
+
+//concise vs. block body 
+//concise body SINGLE PARAMATERS (here, it's (x)) DON'T NEED THE ()
+let apples = (x) => `There are ${x} apples`;
+console.log(apples(10));
+
+//block body SINGLE PARAMATERS (here, it's (x)) DON'T NEED THE ()
+let bananas = (x) => {
+    return `There are ${x} bananas`;
+}
+console.log(bananas(5));
+
+//return must explicitly be written in block-body function
+
+let func = () => 'hi';
+console.log(func());
+
+let secondFunc = () => 'hi';
+console.log(secondFunc());
